9cca3fdc06d6a1a0a2c3d95af53b0a7f6ccabbc1
bluet4805-alt <bluet4805@student.faytechcc.edu>
2026-01-28 15:33:06 +0000

    Started M2lab1

---
+++ M2lab1_Blue.cpp
@@ -0,0 +1,18 @@
+/*
+CIC 134
+M2Lab - The Crate Program 
+Blue 
+1/28/26
+
+Find the volume of a crate 
+Find the cost, customer_price, and profit 
+Print everything out 
+*/
+
+#include <iostream>
+#include <iomanip>
+
+int main() {
+
+    return 0; 
+}
