5939df6d8e8e1842c488ebc3a0db0b062ff666c3
bluet4805-alt <bluet4805@student.faytechcc.edu>
2026-02-14 23:04:14 +0000

    First atempt at Question 1

--- Course-csc-134-26sp/M2/M2HW1.cpp
+++ Course-csc-134-26sp/M2/M2HW1.cpp
@@ -1,6 +1,6 @@
 /*
 CSC 134
-M2HW1- Bronze
+M2HW1- Gold
 Trinity 
 2-11-26
 */
@@ -8,12 +8,34 @@ Trinity
 // Question 1
 
 #include <iostream>
+#include <string>
+
 using namespace std; 
 int main () {
-
     // Variables
+string name;
+double balance, deposit, withdrawl, finalBalance; 
 
+// User Input 
 cout << "Welcome to the Bank of Money!" << endl; 
-cout << "What is your name is your accout under?: " << endl; 
+cout << "Please enter the name your accout is under: " << endl; 
+cin >> name; 
+cout << "Please enter your starting balance:$ ";
+cin >> balance;
+cout << "Perfect! How much would you like to deposit?:$ ";
+cin >> deposit;
+cout << "Now we just need your widthdrawl amount: ";
+cin >> withdrawl; 
 
+// Math
+finalBalance = balance + deposit - withdrawl;
+
+// Display
+cout << "Your Bank Info.";
+cout << endl;
+cout << "_______________________________";
+cout << "Name on account: " << name << endl;
+cout << "Final Balance: $" <<finalBalance << endl;
+
+return 0;
 }
