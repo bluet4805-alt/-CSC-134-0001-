06d6db9c6a4a584762fc5779ab5d5549377fc859
bluet4805-alt <bluet4805@student.faytechcc.edu>
2026-03-04 16:28:13 +0000

    Revison of Test2

---
+++ M3/M3HW1Questions/M3HW1Question2.cpp
@@ -0,0 +1,56 @@
+/*
+CSC 134
+M2T2 - Receipt calculator 
+Blue 
+1-26-26
+Goal: A correct looking recept that handles sales tax. 
+Assumption: Sales tax is 8% 
+*/
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+// if you don't use namespace, type std:: cout evrey time instead of cout (alsp cin)
+
+int main () {
+    // We are making a recept printer for a bookstore 
+// Declare all variables 
+string book_name = "Books R Us      "; // change to anything 
+int num_books;                        // how many they buy 
+double book_price = 5.99;             // $5.99
+double sub_total;                     // price before tax/tips
+double tax_rate = 0.08;               // 8% is 8/100 ("per cent")
+double tip_amount;
+double tax_amount;                    // $ of the actual tax
+double total_price;                   // subtotal + tip + tax       
+
+
+// Get user iuput 
+cout << "Welcome to the CSC 134 Book Store." << endl; 
+cout << "Today's Discount: " << book_name << endl; 
+cout << endl; 
+cout << "How many would you like? ";
+cin >> num_books; 
+cout << "Tip ammount? (min 0)? ";
+cin >> tip_amount; 
+
+// Do the calculations 
+sub_total = book_price * num_books; 
+tax_amount = sub_total * tax_rate; // this much is ADDED to the bill
+// add tax and tips 
+total_price = sub_total + tip_amount +tax_amount; 
+
+// Present the output with 2 decimal palces 
+cout << setprecision(2) << fixed; // remember to import <iomanip>
+cout << endl; 
+cout << "Your Order" << endl << "-----------------------------" << endl; 
+cout << num_books << "x" << book_name << "\t$" << book_price << endl;
+cout << "Subtotal: \t\t$" << sub_total << endl;  
+cout << "Tip: \t\t\t$" << tip_amount << endl;
+cout << "Tax: \t\t\t$" << tax_amount << endl; 
+cout << "-----------------------------" << endl; 
+cout << "Total: \t\t\t$" << total_price << endl; 
+cout << "THANK YOU COME AGAIN!!" << endl; 
+
+    return 0; // no errors
+}
---
+++ M3/M3HW1Questions/M3HWQuestion3.cpp
@@ -0,0 +1,139 @@
+// M3Lab1
+// Blue
+//2-23-26
+// Thank you Youtube and Gemini for helping me remember what the functions do 
+#include <iostream>
+using namespace std; 
+
+// List Prototypes of all choices
+void do_choice_1();
+void do_choice_2();
+
+// main() is here
+int main() {
+
+    int choice;
+    // starts the game
+    cout << "Welcome to Farming Life Storymode! " << endl << endl;
+    cout << "You are working in your cubical and realize that you want more from your life." << endl; //intro and something happens...
+    cout << "You get a call from your mother saying that your grandfather had passed and is giving you his farm!" << endl; 
+    cout << "You quit your job and make your way to the given address." << endl; 
+    cout << "When you arrive you realize how rundown the farm is." << endl; 
+    cout << "You also notice a small town in the distance." << endl; 
+
+    // First Choice 
+    cout << "What is your first decision?" << endl; 
+    cout << "Do You: " << endl; 
+    cout << "1. Explore the Farm?" << endl; 
+    cout << "2. Go to the Town?" << endl; 
+
+    cout << "> "; // Give a prompt to type
+    // Change the names of the choices to make this more organized as we add more 
+    cin >> choice; 
+
+    if (1 == choice) {
+        do_choice_1();
+    }
+    else if (2 == choice) { 
+        do_choice_2();
+    }
+    else {
+        cout << "You answer is not one of the options given." << endl; 
+        cout << "Grandpa's ghost disaproves of your choice and you are forever haunted by his disaproving frown." << endl; // This will prompt if there is an error
+    }
+    cout << "---Game Over---" << endl << endl; // I want this to prompt something more story related 
+
+    return 0; // End of game 
+}
+
+// List full functions of all choices at the bottom 
+void do_choice_1 () {
+    int sub_choice; //I did need to watch youtube vids to understand this function 
+    cout << "As you make your way around the farm you find some tools laying around." << endl;
+    cout << " You see a Hammer and a Sword"<< endl; 
+    cout << "Do you:" << endl; 
+    cout << "1. Pick up the Sword?" << endl; 
+    cout << "2. Pick up the Hammer?" << endl;
+    cout << "> ";
+    cin >> sub_choice; 
+
+    if (1== sub_choice) {
+        cout << "[SYSTEM]: You have added a sword to your inventory" << endl; 
+        cout << "You wander into a cave of dangerous slimes!" << endl; 
+        cout << "Do you:" << endl; 
+        cout << "1. Fight the slimes?" << endl;
+        cout << "2. Run away?" << endl; 
+        cout << "> ";
+        cin >> sub_choice; 
+        // resolve the fight/run decision
+        if (1 == sub_choice) {
+            cout << "You defeat the giant slimes and in doing so you tame the remaining ones!" << endl; 
+            cout << "You now have a slime army!" << endl; 
+            cout <<" [SYSTEM]: +1 Charisma." << endl; 
+        } else {
+            cout << "You get lost in the cave and starve. OH NO!" << endl; 
+        }
+    }
+    else if (2 == sub_choice) {
+            cout << "[SYSTEM]: You have added a Hammer into your inventory." << endl; 
+            cout << "As you continue your exploration you find a rundown barn." << endl; 
+            cout << "Do You: " << endl; 
+            cout << "1. Fix the barn?" << endl; 
+            cout << "2. Keep exploring the farm?" << endl; 
+            cout << "> ";
+            cin >> sub_choice; 
+            if (1 == sub_choice) {
+                cout << "The barn is fixed!" << endl; 
+                cout << "Animals begin to move in and become your friend." << endl; 
+                cout << "[SYSTEM: +8 Charisma]" << endl; 
+            } else {
+                cout << "You leave the barn and continue wandering the land." << endl; 
+                cout <<"As you wander, you begin to feel magic in the air." << endl; 
+                cout << "Looking ahead you see a Unicorn looking for somewhere to keep warm, but because you dont have a barn it begins to look elsewhere as it trots back into the woods" << endl; 
+            }
+        }
+}
+    void do_choice_2() {
+        int sub_choice; 
+        cout << "You are greeted by Mayor Sam and he hands you a box of seeds." << endl; 
+        cout << "How do you reply:" << endl; 
+        cout << "1. 'WoW! This is so thoughtful. Thank you so much!'" << endl; 
+        cout << "2. 'SEEDS?! I JUST GOT HERE AND YOU EXPECT ME TO ALREADY START WORKING ?!'" << endl; 
+        cout << "> "; 
+        cin >> sub_choice; 
+
+        if (1 == sub_choice) {
+            cout << "Sam beams. [SYSTEM]: +3 Charisma." << endl; 
+        } else {
+            cout << "Sam storms off mumbling a threatening remark." << endl; 
+            cout << "[SYSTEM]: You have gained an enemy.  -3 Charisma" << endl; 
+        }
+        cout << "You look around the town and notice two paths." << endl; 
+        cout << "One leads to the town market and the other to the beach." << endl; 
+        cout << "Do you go to:" << endl; 
+        cout << "1. The Beach?" << endl; 
+        cout << "2. The Market?" << endl;
+        cout << "> ";
+        cin >> sub_choice; 
+
+        if (1 == sub_choice) {
+            cout << "A Pirate Captian makes his way to you and instantly approves of you" << endl; 
+            cout << "[SYSTEM]: You are now the first mate to the rugged crew." << endl; 
+        } else if (2 == sub_choice) {
+            cout << "A greedy shop owner pulls you into his booth and asks you to help him take over the town." << endl; 
+            cout << "Do You:" << endl; 
+            cout << "1. Agree to help him?" << endl; 
+            cout << "2. Refuse his offer?" << endl; 
+            cout << "> ";
+            cin >> sub_choice;
+            if (1 == sub_choice) {
+                cout << "You are rich but, the town has become a dystopian society" << endl; 
+                cout << "You sit alone in your cold and empty mansion hiding because the townspeople seek revenge." << endl; 
+            } else{
+                cout << "The townspeople overhear and invite you the guest of honor at their summer fesival!" << endl;
+                cout << "They pitch in to help fix up the farm and you begin to enjoy your new farm life." << endl; 
+                cout << "[SYSTEM]: +50 Charisma and +$40,000,000 from the success of your farm" << endl;  
+            }
+        }
+    }
+// I could eventually have it keep track of different stats such as combat and charisma.
