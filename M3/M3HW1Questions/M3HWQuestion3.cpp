d2a01c1b2341fb1f8cd950803e0c44c07599562b
bluet4805-alt <bluet4805@student.faytechcc.edu>
2026-03-09 14:56:00 +0000

    Polished Test 2

--- M3/M3HW1Questions/M3HWQuestion3.cpp
+++ M3/M3HW1Questions/M3HWQuestion3.cpp
@@ -40,7 +40,9 @@ int main() {
     else {
         cout << "You answer is not one of the options given." << endl; 
         cout << "Grandpa's ghost disaproves of your choice and you are forever haunted by his disaproving frown." << endl; // This will prompt if there is an error
+        cout << "[SYSTEM]: You have gained the Curse Of The Grandfather!!!" << endl; 
     }
+
     cout << "---Game Over---" << endl << endl; // I want this to prompt something more story related 
 
     return 0; // End of game 
@@ -50,7 +52,7 @@ int main() {
 void do_choice_1 () {
     int sub_choice; //I did need to watch youtube vids to understand this function 
     cout << "As you make your way around the farm you find some tools laying around." << endl;
-    cout << " You see a Hammer and a Sword"<< endl; 
+    cout << "You see a Hammer and a Sword"<< endl; 
     cout << "Do you:" << endl; 
     cout << "1. Pick up the Sword?" << endl; 
     cout << "2. Pick up the Hammer?" << endl;
@@ -69,9 +71,9 @@ void do_choice_1 () {
         if (1 == sub_choice) {
             cout << "You defeat the giant slimes and in doing so you tame the remaining ones!" << endl; 
             cout << "You now have a slime army!" << endl; 
-            cout <<" [SYSTEM]: +1 Charisma." << endl; 
+            cout <<" [SYSTEM]: +5 Charisma." << endl; 
         } else {
-            cout << "You get lost in the cave and starve. OH NO!" << endl; 
+            cout << " [SYSTEM]: You get lost in the cave and starve. OH NO!" << endl; 
         }
     }
     else if (2 == sub_choice) {
@@ -90,23 +92,26 @@ void do_choice_1 () {
                 cout << "You leave the barn and continue wandering the land." << endl; 
                 cout <<"As you wander, you begin to feel magic in the air." << endl; 
                 cout << "Looking ahead you see a Unicorn looking for somewhere to keep warm, but because you dont have a barn it begins to look elsewhere as it trots back into the woods" << endl; 
+                cout << "[SYSTEM]: -20 Magic" << endl; 
             }
         }
 }
     void do_choice_2() {
         int sub_choice; 
         cout << "You are greeted by Mayor Sam and he hands you a box of seeds." << endl; 
-        cout << "How do you reply:" << endl; 
+        cout << "How do you reply?:" << endl; 
         cout << "1. 'WoW! This is so thoughtful. Thank you so much!'" << endl; 
         cout << "2. 'SEEDS?! I JUST GOT HERE AND YOU EXPECT ME TO ALREADY START WORKING ?!'" << endl; 
         cout << "> "; 
         cin >> sub_choice; 
 
         if (1 == sub_choice) {
-            cout << "Sam beams. [SYSTEM]: +3 Charisma." << endl; 
+            cout << "Sam smiles as you accept the seeds."<< endl;
+            cout << "You overhear him puting in a good word for you to the rest of the townspeople as you continue walking" << endl; 
+            cout << "[SYSTEM]: +20 Charisma." << endl; 
         } else {
             cout << "Sam storms off mumbling a threatening remark." << endl; 
-            cout << "[SYSTEM]: You have gained an enemy.  -3 Charisma" << endl; 
+            cout << "[SYSTEM]: You have gained an enemy.  -20 Charisma" << endl; 
         }
         cout << "You look around the town and notice two paths." << endl; 
         cout << "One leads to the town market and the other to the beach." << endl; 
@@ -118,7 +123,8 @@ void do_choice_1 () {
 
         if (1 == sub_choice) {
             cout << "A Pirate Captian makes his way to you and instantly approves of you" << endl; 
-            cout << "[SYSTEM]: You are now the first mate to the rugged crew." << endl; 
+            cout << "You are now the first mate to the rugged crew." << endl; 
+            cout << "[SYSTEM]: +50 Bravery" << endl; 
         } else if (2 == sub_choice) {
             cout << "A greedy shop owner pulls you into his booth and asks you to help him take over the town." << endl; 
             cout << "Do You:" << endl; 
@@ -127,12 +133,14 @@ void do_choice_1 () {
             cout << "> ";
             cin >> sub_choice;
             if (1 == sub_choice) {
+                cout << "TIME SKIP 3 YEARS" << endl; 
                 cout << "You are rich but, the town has become a dystopian society" << endl; 
                 cout << "You sit alone in your cold and empty mansion hiding because the townspeople seek revenge." << endl; 
+                cout << "[SYSTEM]: -50 Bravery and -50 Charisma" << endl; 
             } else{
                 cout << "The townspeople overhear and invite you the guest of honor at their summer fesival!" << endl;
                 cout << "They pitch in to help fix up the farm and you begin to enjoy your new farm life." << endl; 
-                cout << "[SYSTEM]: +50 Charisma and +$40,000,000 from the success of your farm" << endl;  
+                cout << "[SYSTEM]: +100 Charisma and +$40,000,000 from the success of your farm" << endl; 
             }
         }
     }
