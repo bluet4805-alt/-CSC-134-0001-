9b63cfc9a8b432062c2ce1d8aaf88c76c69a2c28
bluet4805-alt <bluet4805@student.faytechcc.edu>
2026-03-22 03:35:56 +0000

    M4HW1 Bronze Done

--- M4/M4HW1.cpp
+++ M4/M4HW1.cpp
@@ -1,1 +1,19 @@
-//M4HW1
+// M4HW1
+// Blue
+//3-16-26
+// Gold 
+
+
+#include <iostream>
+using namespace std;
+int main () {
+    //Variables for the table 
+    int tableNum = 5; 
+    int counter = 1; 
+    // Loop 
+    while (counter <= 12) {
+        cout << tableNum << " times " << counter << " is " << (tableNum * counter) << "." << endl; 
+        counter ++; // I looked on Youtube for examples of how to use the ++ function 
+    }
+    return 0;
+}
