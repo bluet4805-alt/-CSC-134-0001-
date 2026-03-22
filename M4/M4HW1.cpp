21f67d1b67707e3075f83e0819290d441be04166
bluet4805-alt <bluet4805@student.faytechcc.edu>
2026-03-22 22:06:13 +0000

    M4HW1 Bronze

--- M4/M4HW1.cpp
+++ M4/M4HW1.cpp
@@ -8,22 +8,12 @@
 using namespace std;
 int main () {
     //Variables
-    int tableNum; 
-    int counter = 1; 
-    int final; 
-    // Ask for number 
-    cout << "Please enter a number from 1-12: ";
-    cin >> tableNum; 
-    // Validation Loop
-    while (tableNum < 1 || tableNum > 12) { // I used google to understand true/false functions and I used the || function
-        cout << "Input is not valid. Please enter a number from 1 to 12: ";
-        cin >> tableNum;
-    }
-    // Loop
-    while (counter <= 12) {
-        final = tableNum * counter;
-        cout << tableNum << " times " << counter << " is " << final << "." << endl; 
-        counter ++;
+    int tableNum = 5; 
+    int counter =1; 
+    //Loop
+    while (counter <=12) {
+        cout << tableNum << " times " << counter << " is " << (tableNum * counter) << "." <<endl; 
+        counter ++; // I looked on youtube for other ways to utilize the ++ funstion 
     }
     return 0;
 }
