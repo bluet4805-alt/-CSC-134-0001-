cefa23aba30a16e37c177c316c1d0ccd4bd1ebd5
bluet4805-alt <bluet4805@student.faytechcc.edu>
2026-03-23 00:11:48 +0000

    M4Lab1 Done

---
+++ M4/M4Lab1.cpp
@@ -0,0 +1,27 @@
+// M4HW1
+// Blue
+//3-17-26
+
+#include <iostream>
+using namespace std; 
+
+int main() {
+    cout << "M4LAB\n";
+
+    // Variables 
+    int height, width; 
+
+    // set the size 
+    height = 5; 
+    width = 5; 
+
+
+    // nested loop
+    for (int j = 0; j < height; j++) {
+        for (int i = 0; i < width; i++) {
+            cout << "* ";
+        }
+        cout << endl; // ends the newer line 
+    }
+    return 0;
+}
