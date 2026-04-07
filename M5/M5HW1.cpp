c8d8e0514b976f6716c849a32f1bd64aea309e32
bluet4805-alt <bluet4805@student.faytechcc.edu>
2026-04-07 10:47:55 -0400

    Starting  M5HW1.cpp

--- M5/M5HW1.cpp
+++ M5/M5HW1.cpp
@@ -1,4 +1,19 @@
 // CSC 134
 // M5HW1
 // Blue
-// 4-1-26
+// 4-1-25
+
+// I originally used the starter code to do this assignment before I began to play with other layouts and found this one to be smoother
+#include <iostream>
+#include <string> 
+#include <iomanip> // I had looked into the setprecision function on a previous assignment and I wanted to play around with it for this one.
+// I did use youtube and google to help me make some notes about its benefits
+using namespace; 
+
+// Prototype
+void question(); 
+
+int main () {
+  question1(); 
+  return 0; 
+}
