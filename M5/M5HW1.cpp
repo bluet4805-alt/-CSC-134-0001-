90ea3ee08a06b15db89b9e0ed1d12d23919a6f8d
bluet4805-alt <bluet4805@student.faytechcc.edu>
2026-04-07 15:08:10 +0000

    FIrst M5HW1 attempt

--- M5/M5HW1.cpp
+++ M5/M5HW1.cpp
@@ -1,9 +1,9 @@
 // CSC 134
 // M5HW1
 // Blue
-// 4-1-25
+// 4-1-26
 
-// I originally used the starter code to do this assignment before I began to play with other layouts and found this one to be smoother
+// I originally used the starter code to do this assignment before I began to play with other layouts and found this one helped me understand the process better 
 #include <iostream>
 #include <string> 
 #include <iomanip> // I had looked into the setprecision function on a previous assignment and I wanted to play around with it for this one.
@@ -16,4 +16,34 @@ void question();
 int main () {
   question1(); 
   return 0; 
+}
+
+void question1 () {
+    // Declare Variables 
+    string month1, month2, month3; 
+    double rain1, rain2, rain3, average; 
+
+    // Get user input 
+    cout << "Please enter a Month: "; 
+    cin >> month1
+    cout << "Now enter rainfall for " << month1 << ": ";
+    cin >> rain1; 
+
+    cout << "Please enter a  second Month: ";
+    cin >> month2; 
+    cout << "Now enter rainfall for " << month2 << ": "; 
+    cin >> rain2; 
+    
+    cout << "Please enter a third month: "; 
+    cin >> month3; 
+    cout << "Now enter rainfall for " << month3 << ": ";
+    cin >> rain3; 
+
+    // Calculate the average 
+    average = (rain1 + rain2 + rain3) 3.0; 
+
+    // Display an output with 2 decimal places 
+    cout << fixed << setprecision (2); 
+// I still dont completely understand the "fixed" and "setprecision" funstions so I am using my time now to play around with them 
+    cout << "The average rainfall for " << month1 << ", " << month2 << ", and " << month3 << " is " << average << " inches." << endl; 
 }
