84f08e7985f3049e8bbde7f14f9c1e4d285f8a57
bluet4805-alt <bluet4805@student.faytechcc.edu>
2026-04-29 15:29:46 +0000

    started M7T2

--- M7/M7T2.cpp
+++ M7/M7T2.cpp
@@ -7,13 +7,24 @@
 #include <iostream>
 using namespace std; 
 
+void setName(string &name);
+
 int main (){
+
     string name = "Bob"; 
     string * pName =&name; // pointer, which points to address of name 
+
+    setName(name);
 
     cout << "name  = " << name << endl; 
     cout << "pName = " << pName << endl; 
     cout << "*pName = " << *pName << endl; 
 
     return 0;
+}
+
+void setName(string &name) {
+    // uses pass-by-refrence, changing the original 
+    cout << "What's your name? ";
+    cin >> name; 
 }
